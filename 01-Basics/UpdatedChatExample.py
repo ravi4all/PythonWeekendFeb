6f6783f3b53f73e33b2a75265117728431f42175
ravi4all <ravikanttyagi.2295@gmail.com>
2017-02-20 18:03:54 +0530

    Add files via upload

---
+++ 01-Basics/UpdatedChatExample.py
@@ -0,0 +1,21 @@
+import random
+
+a = ['Hi', 'Greetings', 'Good Morning', 'Have a nice day']
+b = random.choice(a)
+
+user_name = input("Enter your name : ")
+print(b,user_name)
+
+
+while True:
+    user_message = input("Enter your message :")
+    print("User's Message",user_message)
+
+    if user_message == 'Hi' or user_message == 'hi':
+        print("Bot's Message",a[0])
+
+    elif user_message == 'Bye' or user_message == 'bye':
+        print('Byeeeee')
+
+    else:
+        print(random.choice(a))
